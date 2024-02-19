a864411e8c7879ae1b2b60add0cef4f02ec3774f
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-02-19 18:19:13 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
@@ -13,7 +12,7 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
 
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: 'main.js',
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
